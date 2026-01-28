e6103c0da6443bffb0763280d3ac2682fcaf231d
vvempala-vamsi <vvempala@student.gitam.edu>
2026-01-28 11:33:25 +0530

    Create PASS_BREAK.py

---
+++ PASS_BREAK.py
@@ -0,0 +1,23 @@
+def show_numbers():
+    for i in range(-1, 11):
+
+        if i == 3:
+            pass        # does nothing
+
+        elif i == 4:
+            break       # stops the loop
+
+        print(i)
+
+
+# function call
+show_numbers()
+
+# OUTPUT
+-1
+0
+1
+2
+3
+
+=== Code Execution Successful ===

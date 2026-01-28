d47041627546176eb7cf25d03d960d004ba01907
vvempala-vamsi <vvempala@student.gitam.edu>
2026-01-28 11:06:35 +0530

    Add loop example with pass, continue, and break
    Demonstrate usage of pass, continue, and break in a loop.

---
+++ PASS_CONTINUE_BREAK.py
@@ -0,0 +1,21 @@
+for i in range(1,11):
+    if i == 3:
+        pass        
+    elif i == 5:
+        continue   
+    elif i == 10:
+        break      
+    print(i)
+
+# OUTPUT
+
+1
+2
+3
+4
+6
+7
+8
+9
+
+=== Code Execution Successful ===

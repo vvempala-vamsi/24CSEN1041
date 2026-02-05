7a73c11fcacd3245eccd7d5901f4ec155a9847e0
vvempala-vamsi <vvempala@student.gitam.edu>
2026-02-05 15:18:55 +0530

    Create STRING_LENGTH.py

---
+++ STRING_LENGTH.py
@@ -0,0 +1,13 @@
+s = input("enter the string : ")
+count=0
+for ch in s:
+ count+=1
+ 
+print("length",count)
+
+#OUTPUT
+
+enter the string : VAMSI VISHAL
+length 12
+
+=== Code Execution Successful ===

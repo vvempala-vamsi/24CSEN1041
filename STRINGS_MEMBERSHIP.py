191f8954d89a124deb28e8a881c84c9eb8487a5a
vvempala-vamsi <vvempala@student.gitam.edu>
2026-02-05 15:42:57 +0530

    Create STRINGS_MEMBERSHIP.py

---
+++ STRINGS_MEMBERSHIP.py
@@ -0,0 +1,20 @@
+N = ("KOHLI","DHONI","ROHIT","SIRAJ","DUBE","SACHIN","JADDU","SURYA","ARJUN","BEN")
+
+
+print("STOKES" in N)
+print("MSD" not in N)
+print("KOHLI" in N)
+print("KUMAR" not in N)
+print("PAYAL" in N)
+print(" UPPAL BALU" in N)
+
+#OUTPUT
+
+False
+True
+True
+True
+False
+False
+
+=== Code Execution Successful ===

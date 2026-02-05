5c5cc30aa34f0db87572d81449fca3fe2d443de9
vvempala-vamsi <vvempala@student.gitam.edu>
2026-02-05 14:51:59 +0530

    Create STRINGS_SLICING.py

---
+++ STRINGS_SLICING.py
@@ -0,0 +1,19 @@
+# 1. String
+s = "markram"
+
+# 2. Length of string
+print(len(s))
+
+# 3. Access character
+print(s[0])      # first character
+print(s[-2:])     # last character
+
+print(s[::-1])
+
+#OUTPUT
+7 = NO OF LETTERS
+m = 1ST LETTER
+am = LAST TWO LETTERS OF THE WORD GIVEN
+markram
+
+=== Code Execution Successful ===
